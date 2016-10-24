68ffeeda4bfaf725da7ad4ab350715937b6b10cb
dillon <dilloncordovajunkmail@gmail.com>
2016-10-24 13:07:03 -0700

    Initial commit

---
+++ app/scripts/libs/Assert.js
@@ -0,0 +1,26 @@
+/**
+ * Created by dillon_cordova on 1/4/2016.
+ */
+var Assert = (function() {
+	var isDevelopModeOn = false;
+	var isDebugModeOn = true;
+
+	return {
+		is: function(_condition, _message) {
+			if(isDebugModeOn && !_condition) {
+				throw new Error(_message);
+			}
+		},
+		console: function(_message, _condition) {
+			if(isDevelopModeOn && (_condition || _condition === undefined)) {
+				console.log(_message);
+			}
+		},
+		setDebugModeTo: function(_bool) {
+			isDebugModeOn = _bool;
+		},
+		setDevelopModeTo: function(_bool) {
+			isDevelopModeOn = _bool;
+		}
+	};
+})();
